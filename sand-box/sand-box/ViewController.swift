44488ddd81237dd640b64e787fcbb111c5dd5515
Rodrigo Queiroz <rodrigogqueiroz@live.com>
2024-03-14 15:53:47 -0300

    add loading view

--- sand-box/sand-box/ViewController.swift
+++ sand-box/sand-box/ViewController.swift
@@ -31,39 +31,27 @@ var productsDataStore: [ProductsMock] = [
         imageURL: "https://cdn-icons-png.flaticon.com/512/6708/6708069.png",
         description: "")
 ]
-/*
- 
- 
- {
-   "name": "XBOX",
-   "imageURL": "https://s3-sa-east-1.amazonaws.com/digio-exame/xbox_icon.png",
-   "description": "Com o e-Gift Card Xbox você adquire créditos para comprar games, música, filmes, programas de TV e muito mais!"
- },
- {
-   "name": "Google Play",
-   "imageURL": "https://s3-sa-east-1.amazonaws.com/digio-exame/google_play_icon.png",
-   "description": "O e-gift Google Play dá acesso a um mundo de entretenimento. É muito fácil encontrar algo que você goste, seja você um fã de música ou de filmes, livros, revistas, apps ou jogos. Com o e-gift Google Play, você não precisa de cartão de crédito e pode aproveitar o melhor do cinema, suas músicas favoritas e muito mais em smartphones e tablets Android, iOS e também no computador."
- },
- {
-   "name": "Level up",
-   "imageURL": "https://s3-sa-east-1.amazonaws.com/digio-exame/level_up_icon.png",
-   "description": "Com o e-Gift Card Level Up você adquire créditos para jogar diversas opções de games, de MMORPGs a jogos de Tiro e Mobas! Para ver a lista de jogos e a quantia de créditos que pode resgatar em cada um deles, acesse: http://levelupgames.uol.com.br/levelup/seuegiftcard.lhtml"
- }
- **/
-
-
-
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var viewLoading: UIView!
+  @IBOutlet weak var contentView: UIView!
+  
   @IBOutlet weak var mainTableView: UITableView!
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      self.viewLoading.isHidden = true
+      self.contentView.isHidden = false
+    }
   }
   
-  
+  override func viewWillAppear(_ animated: Bool) {
+    viewLoading.isHidden = false
+    contentView.isHidden = true
+  }
 }
 
 extension ViewController: UITableViewDelegate {
