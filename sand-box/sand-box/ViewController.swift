0971a6fd3449176919f30750613bcfc3070c9abb
Rodrigo Queiroz <rodrigogqueiroz@live.com>
2024-03-13 22:08:12 -0300

    create project

---
+++ sand-box/sand-box/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sand-box
+//
+//  Created by Rodrigo Queiroz on 13/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
