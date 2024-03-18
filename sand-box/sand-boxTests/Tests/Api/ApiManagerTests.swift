e1d3a6503008a1ca86666248ab814dc8016a7a86
Rodrigo Queiroz <rodrigogqueiroz@live.com>
2024-03-18 19:55:48 -0300

    Fix unit test

--- sand-box/sand-boxTests/Tests/Api/ApiManagerTests.swift
+++ sand-box/sand-boxTests/Tests/Api/ApiManagerTests.swift
@@ -8,53 +8,59 @@
 import XCTest
 
 final class ApiManagerTests: XCTestCase {
+  
+  override func tearDown() {
+    super.tearDown()
+    APIManager.shared.url = URL(string: "https://7hgi9vtkdc.execute-api.sa-east-1.amazonaws.com/sandbox/products") ??  URL(filePath: "")
+  }
+  
   func testFetchData_withSuccess() {
-      //given
-      let expectation = XCTestExpectation(description: "Data fetched")
-      let sut = APIManager.shared
-      var product: ProductData?
-      
-      //when
-      sut.fetchData { result in
-          switch result {
-          case .success(let products):
-              expectation.fulfill()
-              product = products
-          case .failure(let failure):
-              expectation.fulfill()
-              XCTFail("It should have fetched correctly, instead error \(failure) ocurred")
-          }
+    //given
+    let expectation = XCTestExpectation(description: "Data fetched")
+    let sut = APIManager.shared
+    var product: ProductData?
+    
+    //when
+    sut.fetchData { result in
+      switch result {
+        case .success(let products):
+          expectation.fulfill()
+          product = products
+        case .failure(let failure):
+          expectation.fulfill()
+          XCTFail("It should have fetched correctly, instead error \(failure) ocurred")
       }
-      
-      //then
-      wait(for: [expectation], timeout: 5.0)
-      XCTAssertNotNil(product!, "should be filled on the fetch data")
-      XCTAssertNotNil(product?.cash)
-      XCTAssertNotNil(product?.products)
-      XCTAssertNotNil(product?.spotlights)
+    }
+    
+    //then
+    wait(for: [expectation], timeout: 5.0)
+    //XCTAssertNotNil(product, "should be filled on the fetch data")
+    XCTAssertTrue(product != nil)
+    XCTAssertNotNil(product?.products)
+    XCTAssertNotNil(product?.spotlights)
   }
   
   func testFetchData_withFailure() {
-      //given
-      let expectation = XCTestExpectation(description: "Data not fetched")
-      APIManager.shared.url = URL(string: "") ?? URL(filePath: "")
-      let sut = APIManager.shared
-      var product: ProductData?
-      
-      //when
-      sut.fetchData { result in
-          switch result {
-          case .success(let products):
-              expectation.fulfill()
-              product = products
-              XCTFail("It should have fetched correctly, instead error ocurred")
-          case .failure(_):
-              expectation.fulfill()
-          }
+    //given
+    let expectation = XCTestExpectation(description: "Data not fetched")
+    APIManager.shared.url = URL(string: "") ?? URL(filePath: "")
+    let sut = APIManager.shared
+    var product: ProductData?
+    
+    //when
+    sut.fetchData { result in
+      switch result {
+        case .success(let products):
+          expectation.fulfill()
+          product = products
+          XCTFail("It should have fetched correctly, instead error ocurred")
+        case .failure(_):
+          expectation.fulfill()
       }
-      
-      //then
-      wait(for: [expectation], timeout: 5.0)
-      XCTAssertNil(product)
+    }
+    
+    //then
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertNil(product)
   }
 }
--- sand-box/sand-boxTests/Tests/Home/HomeViewModelTests.swift
+++ sand-box/sand-boxTests/Tests/Home/HomeViewModelTests.swift
@@ -8,54 +8,62 @@
 import XCTest
 
 final class HomeViewModelTests: XCTestCase {
-    func testFetchProducts_WithSuccess() {
-      //given
-      let expectation = XCTestExpectation(description: "Data fetched successfully")
-      let sut = HomeViewModel()
-      
-      //when
-      DispatchQueue.main.async {
-          sut.fetchProducts { result in
-              switch result {
-              case .success():
-                  expectation.fulfill()
-              case .failure(let failure):
-                  expectation.fulfill()
-                  XCTFail("\(failure)")
-              }
-          }
+  
+  var sut: HomeViewModel!
+  
+  override func setUp() {
+    super.setUp()
+    sut = HomeViewModel()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+    sut = nil
+    APIManager.shared.url = URL(string: "https://7hgi9vtkdc.execute-api.sa-east-1.amazonaws.com/sandbox/products") ??  URL(filePath: "")
+  }
+  
+  func testFetchProducts_WithSuccess() {
+    //given
+    let expectation = XCTestExpectation(description: "Data fetched successfully")
+    
+    //when
+    sut.fetchProducts { result in
+      switch result {
+        case .success():
+          expectation.fulfill()
+        case .failure(let failure):
+          expectation.fulfill()
+          XCTFail("\(failure)")
       }
-      
-      //then
-      wait(for: [expectation], timeout: 5.0)
-      XCTAssertNotNil(sut.cash!)
-      XCTAssertNotNil(sut.productsCollectionDataSource.models)
-      XCTAssertNotNil(sut.spotlightCollectionDataSource.models)
-
     }
     
-    func testFetchProducts_WithFailure() {
-        //given
-        let expectation = XCTestExpectation(description: "Data not fetched successfully")
-        let sut = HomeViewModel()
-        APIManager.shared.url = URL(string: "") ?? URL(filePath: "")
-        
-        //when
-        DispatchQueue.main.async {
-            sut.fetchProducts { result in
-                switch result {
-                case .success():
-                    XCTFail("Should not have been a success")
-                case .failure(_):
-                    expectation.fulfill()
-                }
-            }
-        }
-        
-        //then
-        wait(for: [expectation], timeout: 5.0)
-        XCTAssertNil(sut.cash)
-        XCTAssertEqual(sut.productsCollectionDataSource.models.isEmpty, true)
-        XCTAssertEqual(sut.spotlightCollectionDataSource.models.isEmpty, true)
+    //then
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertNotNil(sut.cash)
+    XCTAssertNotNil(sut.productsCollectionDataSource.models)
+    XCTAssertNotNil(sut.spotlightCollectionDataSource.models)
+    
+  }
+  
+  func testFetchProducts_WithFailure() {
+    //given
+    let expectation = XCTestExpectation(description: "Data not fetched successfully")
+    APIManager.shared.url = URL(string: "") ?? URL(filePath: "")
+    
+    //when
+    sut.fetchProducts { result in
+      switch result {
+        case .success():
+          XCTFail("Should not have been a success")
+        case .failure(_):
+          expectation.fulfill()
+      }
     }
+    
+    //then
+    wait(for: [expectation], timeout: 5.0)
+    XCTAssertNil(sut.cash)
+    XCTAssertEqual(sut.productsCollectionDataSource.models.isEmpty, true)
+    XCTAssertEqual(sut.spotlightCollectionDataSource.models.isEmpty, true)
+  }
 }
